422453a125f75fa3a4ab35a3bb95f2ac73a1241f
Leon Ng <lng103@ucsc.edu>
2024-11-27 16:44:00 -0800

    Update main.js

--- docs/pakupaku/main.js
+++ docs/pakupaku/main.js
@@ -1,4 +1,4 @@
-title = "PAKU PAKU";
+title = 'PAKU PAKU';
 
 description = `
 [Tap] Turn
@@ -64,18 +64,18 @@ llll
 ];
 
 options = {
-  theme: "dark",
-  viewSize: { x: 100, y: 50 },
+  theme: 'dark',
+  viewSize: {x: 100, y: 100},
   isPlayingBgm: true,
   isReplayEnabled: true,
   seed: 9,
 };
 
-/** @type {{x: number, vx: number}} */
+/** @type {{y: number, vy: number}} */
 let player;
-/** @type {{x: number, eyeVx: number}} */
+/** @type {{y: number, eyeVy: number}} */
 let enemy;
-/** @type {{x: number, isPower: boolean}[]} */
+/** @type {{y: number, isPower: boolean}[]} */
 let dots;
 let powerTicks;
 let animTicks;
@@ -83,107 +83,109 @@ let multiplier;
 
 function update() {
   if (!ticks) {
-    player = { x: 40, vx: 1 };
-    enemy = { x: 100, eyeVx: 0 };
+    player = {y: 40, vy: 1};
+    enemy = {y: 40, eyeVy: 0};
     multiplier = 0;
     addDots();
     powerTicks = animTicks = 0;
   }
   animTicks += difficulty;
-  color("black");
-  text(`x${multiplier}`, 3, 9);
+  color('black');
+  text(`y${multiplier}`, 3, 9);
+
+
+
   if (input.isJustPressed) {
-    player.vx *= -1;
+    player.vy *= -1;
   }
-  player.x += player.vx * 0.5 * difficulty;
-  if (player.x < -3) {
-    player.x = 103;
-  } else if (player.x > 103) {
-    player.x = -3;
+  player.y += player.vy * 0.5 * difficulty;
+  if (player.y < 3) {
+    player.y = 103;
+  } else if (player.y > 100) {
+    player.y = -3;
   }
-  color("blue");
-  rect(0, 23, 100, 1);
-  rect(0, 25, 100, 1);
-  rect(0, 34, 100, 1);
-  rect(0, 36, 100, 1);
-  color("green");
+
+
+
+  color('blue');
+  rect(33, 0, 1, 100);
+  rect(35, 0, 1, 100);
+  rect(44, 0, 1, 100);
+  rect(46, 0, 1, 100);
+  color('green');
   const ai = floor(animTicks / 7) % 4;
-  char(addWithCharCode("a", ai === 3 ? 1 : ai), player.x, 30, {
+  char(addWithCharCode('a', ai === 3 ? 1 : ai), player.y, 30, {
     // @ts-ignore
-    mirror: { x: player.vx },
+    mirror: {x: player.vy},
   });
+
   remove(dots, (d) => {
     color(
-      d.isPower && floor(animTicks / 7) % 2 === 0 ? "transparent" : "yellow"
-    );
-    const c = char(d.isPower ? "g" : "f", d.x, 30).isColliding.char;
+        d.isPower && floor(animTicks / 7) % 2 === 0 ? 'transparent' : 'yellow');
+    const c = char(d.isPower ? 'g' : 'f', d.y, 30).isColliding.char;
     if (c.a || c.b || c.c) {
       if (d.isPower) {
-        play("jump");
-        if (enemy.eyeVx === 0) {
+        play('jump');
+        if (enemy.eyeVy === 0) {
           powerTicks = 120;
         }
       } else {
-        play("hit");
+        play('hit');
       }
       addScore(multiplier);
       return true;
     }
   });
-  const evx =
-    enemy.eyeVx !== 0
-      ? enemy.eyeVx
-      : (player.x > enemy.x ? 1 : -1) * (powerTicks > 0 ? -1 : 1);
-  enemy.x = clamp(
-    enemy.x +
-      evx *
-        (powerTicks > 0 ? 0.25 : enemy.eyeVx !== 0 ? 0.75 : 0.55) *
-        difficulty,
-    0,
-    100
-  );
-  if ((enemy.eyeVx < 0 && enemy.x < 1) || (enemy.eyeVx > 0 && enemy.x > 99)) {
-    enemy.eyeVx = 0;
+  const eyeVy = enemy.eyeVy !== 0 ?
+      enemy.eyeVy :
+      (player.y > enemy.y ? 1 : -1) * (powerTicks > 0 ? -1 : 1);
+  enemy.y = clamp(
+      enemy.y +
+          eyeVy *
+              (powerTicks > 0        ? 0.25 :
+                   enemy.eyeVy !== 0 ? 0.75 :
+                                       0.55) *
+              difficulty,
+      0, 100);
+
+  if ((enemy.eyeVy < 0 && enemy.y < 1) || (enemy.eyeVy > 0 && enemy.y > 99)) {
+    enemy.eyeVy = 0;
   }
   color(
-    powerTicks > 0
-      ? powerTicks < 30 && powerTicks % 10 < 5
-        ? "black"
-        : "blue"
-      : enemy.eyeVx !== 0
-      ? "black"
-      : "red"
-  );
-  const c = char(
-    enemy.eyeVx !== 0 ? "h" : addWithCharCode("d", floor(animTicks / 7) % 2),
-    enemy.x,
-    30,
-    {
-      // @ts-ignore
-      mirror: { x: evx },
-    }
-  ).isColliding.char;
-  if (enemy.eyeVx === 0 && (c.a || c.b || c.c)) {
+      powerTicks > 0 ?
+          powerTicks < 30 && powerTicks % 10 < 5 ? 'black' : 'blue' :
+          enemy.eyeVy !== 0 ? 'black' :
+                              'red');
+  const c =
+      char(
+          enemy.eyeVy !== 0 ? 'h' :
+                              addWithCharCode('d', floor(animTicks / 7) % 2),
+          enemy.y, 30, {
+            // @ts-ignore
+            mirror: {x: evx},
+          })
+          .isColliding.char;
+  if (enemy.eyeVy === 0 && (c.a || c.b || c.c)) {
     if (powerTicks > 0) {
-      play("powerUp");
-      addScore(10 * multiplier, enemy.x, 30);
-      enemy.eyeVx = player.x > 50 ? -1 : 1;
+      play('powerUp');
+      addScore(10 * multiplier, enemy.y, 30);
+      enemy.eyeVy = player.y > 50 ? -1 : 1;
       powerTicks = 0;
       multiplier++;
     } else {
-      play("explosion");
+      play('explosion');
       end();
     }
   }
   powerTicks -= difficulty;
   if (dots.length === 0) {
-    play("coin");
+    play('coin');
     addDots();
   }
 }
 
 function addDots() {
-  let pi = player.x > 50 ? rndi(1, 6) : rndi(10, 15);
-  dots = times(16, (i) => ({ x: i * 6 + 5, isPower: i === pi }));
+  let pi = player.y > 50 ? rndi(1, 6) : rndi(10, 15);
+  dots = times(16, (i) => ({x: i * 6 + 5, isPower: i === pi}));
   multiplier++;
 }
